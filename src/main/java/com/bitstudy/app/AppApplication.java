e18cd588646f3e745969022438b459a579ec18a4
yjeigno <102030510+yjeigno@users.noreply.github.com>
2023-01-04 20:45:12 +0900

    #23 - 게시판 페이지에 decoupled logic 적용
    타임리프 문법을 HTML 마크업 페이지에서 분리해서 th.xml 파일로 옮김

--- src/main/java/com/bitstudy/app/AppApplication.java
+++ src/main/java/com/bitstudy/app/AppApplication.java
@@ -2,8 +2,10 @@ package com.bitstudy.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bitstudy/app/controller/Ex08_4_ArticleController.java
+++ src/main/java/com/bitstudy/app/controller/Ex08_4_ArticleController.java
@@ -17,12 +17,16 @@ import java.util.List;
  */
 
 @Controller
-@RequestMapping("/articles") // 모든 경로들은 /articles 로 시작하니까 클래스 레벨이 1자로 @RequestMapping("/articles") 걸어 놨음
+@RequestMapping("/articles08") // 모든 경로들은 /articles 로 시작하니까 클래스 레벨에 1차로 @RequestMapping("/articles") 걸어놨음
 public class Ex08_4_ArticleController {
 
     @GetMapping
     public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+        /* ModelMap: 테스트파일에서 .andExpect(model().attributeExists("articles")); 를 이용해서 articles 라는 키값으로 데이터를 넣어주기로 했으니까 필요함.
+
+         * Model 과 ModelMap 은 같은 거임. 차이점은 Model 은 인터페이스, ModelMap 은 클래스(구현체)
+         */
+        map.addAttribute("articles", List.of()); // 키: articles, 값: 그냥 list
         return "articles/index";
     }
 
